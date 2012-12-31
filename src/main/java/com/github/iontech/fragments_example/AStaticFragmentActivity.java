61fbde46c8476d0e121b00563708b191bc8548fa
iontech <ikram@agiliq.com>
2012-12-31 08:13:51 +0530

    Static way of making an Activity to host the fragment.

---
+++ src/main/java/com/github/iontech/fragments_example/AStaticFragmentActivity.java
@@ -0,0 +1,12 @@
+package com.github.iontech.fragments_example;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class AStaticFragmentActivity extends FragmentActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static);
+    }
+}
